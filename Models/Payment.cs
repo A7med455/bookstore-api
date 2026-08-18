f0c7d42f05eeec257cdcc1f4872d053612789881
Ahmed Sameh <AhmedSameh2006ASF@gmail.com>
2026-08-18 21:00:41 +0300

    initial Payment Model Class

---
+++ Models/Payment.cs
@@ -0,0 +1,26 @@
+
+namespace BookStoreAPI.Models
+{
+    public enum PaymentMethod
+    {//only one payment method for now
+        CreditCard
+    }
+    public enum PaymentStatus
+    {
+        Pending,
+        Completed,
+        Failed,
+        Refunded   
+    }
+    public class Payment
+    {
+        public int PaymentId{ get; set;}
+        public required int OrderId{ get; set;}
+        public required decimal Amount{ get; set;}
+        public required PaymentMethod PaymentMethod{ get; set;}
+        public required PaymentStatus PaymentStatus{ get; set;}
+        public required DateTime TransactionDate{ get; set;}
+        public required string TransactionReference { get; set; }
+        
+    }
+}
