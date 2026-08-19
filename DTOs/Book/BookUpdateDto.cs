5f57d806096f59cab7ddf141194376cc347b12f0
Ahmed Sameh <AhmedSameh2006ASF@gmail.com>
2026-08-19 17:40:18 +0300

    initial BookUpdateDto Class

---
+++ DTOs/Book/BookUpdateDto.cs
@@ -0,0 +1,13 @@
+namespace BookStoreAPI.DTOs.Book
+{
+    public class BookUpdateDto
+    {
+        public required int CategoryId{ get; set;} 
+        public required int AuthorId{ get; set;}  
+        public required string Title{ get; set;}
+        public required string ISBN{ get; set;} 
+        public required int Stock{ get; set;}
+        public required decimal Price{get; set;}
+        public required DateTime PublishedDate{ get; set;}
+    }
+}
