beec4ee903ccee6dca89ec83c666a8210c50b49a
Ahmed Sameh <AhmedSameh2006ASF@gmail.com>
2026-08-19 17:40:38 +0300

    updated

--- Models/Book.cs
+++ Models/Book.cs
@@ -8,7 +8,7 @@ namespace BookStoreAPI.Models
         public required string Title{ get; set;}
         public required string ISBN{ get; set;} 
         public required int Stock{ get; set;}
-        public required double Price{get; set;}
+        public required decimal Price{get; set;}
         public required DateTime PublishedDate{ get; set;}
 
         
