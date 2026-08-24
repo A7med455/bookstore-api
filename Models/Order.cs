a99c7ca0705598cd4fa8af989198e46d87e32ed0
Ahmed Sameh <AhmedSameh2006ASF@gmail.com>
2026-08-25 00:45:30 +0300

    a list of OrderItems Added

--- Models/Order.cs
+++ Models/Order.cs
@@ -12,7 +12,7 @@ namespace BookStoreAPI.Models
         public int OrderId{ get; set;} //auto incremented in DB
         public required int CustomerId{ get; set;}//a copy  is required when creating an obj
         public required DateTime OrderDate{ get; set;}
-        public required Status OrderStatus{ get; set;} // how it is gonna be tracked it will be changing from state to state 
-
+        public required Status OrderStatus{ get; set;} 
+        public List<OrderItem> Items{ get; set;} = new(); // when a new Order is created immediately give Items an empty list so start with 
     }
 }
