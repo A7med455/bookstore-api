13954e886bbed3c7673d482be4d7f7b476ebf6f5
Ahmed Sameh <AhmedSameh2006ASF@gmail.com>
2026-08-25 01:14:09 +0300

    applying navigation property by adding Author and Category

--- Models/Book.cs
+++ Models/Book.cs
@@ -3,8 +3,10 @@ namespace BookStoreAPI.Models
     public class Book
     {
         public  int  BookId{ get; set;} //auto incremented (later)
-        public required int CategoryId{ get; set;} 
-        public required int AuthorId{ get; set;}  
+        public required int CategoryId{ get; set;}
+        public required int AuthorId{ get; set;}
+        public Category? Category{ get; set;}
+        public Author? Author{ get; set;}
         public required string Title{ get; set;}
         public required string ISBN{ get; set;} 
         public required int Stock{ get; set;}
