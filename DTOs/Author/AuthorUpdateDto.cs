0abcb75cca10d7a9ded20d72a4759bd63cf8a04d
Ahmed Sameh <AhmedSameh2006ASF@gmail.com>
2026-08-29 23:23:45 +0300

    AuthorUpdateDto Added

---
+++ DTOs/Author/AuthorUpdateDto.cs
@@ -0,0 +1,9 @@
+namespace BookStoreAPI.DTOs.Author
+{
+    public class AuthorUpdateDto
+    {
+        public string? Name{ get; set;}
+        public string? Bio{ get; set;}
+        public int? Age{ get; set;}
+    }
+}
