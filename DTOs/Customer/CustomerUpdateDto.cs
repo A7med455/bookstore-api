1039fa604ffeac2a689a95940539cff587aa7a27
Ahmed Sameh <AhmedSameh2006ASF@gmail.com>
2026-08-30 19:35:35 +0300

    CustomerUpdateDto Added

---
+++ DTOs/Customer/CustomerUpdateDto.cs
@@ -0,0 +1,9 @@
+namespace BookStoreAPI.DTOs.Customer
+{
+    public class CustomerUpdateDto
+    {
+        public string? AccountUserName{ get; set;}
+        public string? Name{ get; set;}
+        public int? Age{ get; set;}
+    }
+}
